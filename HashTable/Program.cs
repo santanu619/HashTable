710214b892b30b6b7d9eb3850092cdd255e1a01f
santanu619 <mohapatra.santanu123@gmail.com>
2021-05-17 11:51:30 +0530

    Added: Code for Hash Tables

---
+++ HashTable/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HashTableCsharp
+{
+    class Program
+    {
+        public static void CountWordFrequency(string sentence, MyMapNode<string, int> frequencyDictionary)
+        {
+            string[] words = sentence.Split(' ');
+            foreach (string word in words)
+            {
+                if (frequencyDictionary.Exists(word))
+                {
+                    frequencyDictionary.Add(word, frequencyDictionary.Get(word) + 1);
+                }
+                else
+                {
+                    frequencyDictionary.Add(word, 1);
+                }
+            }
+            frequencyDictionary.PrintDictionary();
+            Console.WriteLine("---------------------------------");
+
+        }
+        static void Main(string[] args)
+        {
+            MyMapNode<string, int> frequencyDictionaryLine1 = new MyMapNode<string, int>(10);
+            MyMapNode<string, int> frequencyDictionaryParanoid = new MyMapNode<string, int>(10);
+            CountWordFrequency("To be or not to be", frequencyDictionaryLine1);
+            CountWordFrequency("Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations", frequencyDictionaryParanoid);
+            frequencyDictionaryParanoid.Remove("avoidable");
+            frequencyDictionaryParanoid.PrintDictionary();
+        }
+    }
+}
